76084416f7feb1cbe1dca51505afc64619c7e646
SabanaSK <Sornkarn_su@hotmail.com>
2023-11-13 12:56:17 +0100

    Remove verify admin for validateInviteToken

--- routes/inviteTokensRouter.js
+++ routes/inviteTokensRouter.js
@@ -6,11 +6,7 @@ const router = express.Router();
 
 //Added as Admin for now but the endpoint might be deleted
 router.get("/", verify.verifyAdmin, inviteTokensControllers.getAllInviteToken);
-router.get(
-  "/validateInviteToken",
-  verify.verifyAdmin,
-  inviteTokensControllers.validateInviteToken
-);
+router.get("/validateInviteToken", inviteTokensControllers.validateInviteToken);
 router.delete(
   "/:id",
   verify.verifyAdmin,
