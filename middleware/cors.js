0d5ee1c1bc61f4a73d9c143224547acde48b10ce
SabanaSK <Sornkarn_su@hotmail.com>
2023-11-15 10:11:26 +0100

    Refactor Cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -4,6 +4,10 @@ dotenv.config();
 
 const corsOptions = {
   origin: `http://localhost:${process.env.PORT}`,
+  exposedHeaders: ["Authorization"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
+  allowedHeaders: ["Content-Type", "Authorization", "Origin", "X-Auth-Token"],
 };
 
 export default corsOptions;
